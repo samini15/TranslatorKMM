307bd7f1c91b78d41303d89905f40132f3524364
Shayan <shayan.amini76@gmail.com>
2024-04-02 15:19:49 +0200

    Implement Translation History list

---
+++ iosApp/iosApp/translate/presentation/components/TranslateHistoryItem.swift
@@ -0,0 +1,19 @@
+//
+//  TranslateHistoryItem.swift
+//  iosApp
+//
+//  Created by Shayan Amini on 02/04/2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct TranslateHistoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslateHistoryItem()
+}
