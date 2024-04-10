fd070faab15a7ff699074baaaae7686dccb63981
Shayan <shayan.amini76@gmail.com>
2024-04-10 15:55:33 +0200

    VoiceToTextParser

---
+++ iosApp/iosApp/voice_to_text/data/IOSVoiceToTextParser.swift
@@ -0,0 +1,9 @@
+//
+//  IOSVoiceToTextParser.swift
+//  iosApp
+//
+//  Created by Shayan Amini on 08/04/2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import Foundation
---
+++ iosApp/iosApp/voice_to_text/data/MicrophonePowerObserver.swift
@@ -0,0 +1,9 @@
+//
+//  MicrophonePowerObserver.swift
+//  iosApp
+//
+//  Created by Shayan Amini on 06/04/2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import Foundation
